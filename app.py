e43e6442259bdf5f2b40a566b65849d70e9ea847
Divyraj <divyrajchavda1987@gmail.com>
2025-11-11 15:21:33 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -18,5 +18,8 @@ def submit():
 
     return "✅ Data saved successfully!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
